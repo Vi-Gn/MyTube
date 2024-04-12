b5ee780f33fb168d0e8032f1859e6c0f51dcd2ba
Vi-Gn <gnwarem9@gmail.com>
2024-04-12 01:13:21 +0000

    Done first video section first reel section

---
+++ src/js/app.js
@@ -0,0 +1,172 @@
+
+
+"use strict"
+// window.addEventListener('load', main);
+// Everything shall be inside main()
+
+
+
+
+class VideoObj {
+  constructor(
+    src_thumbnail = null,
+    src_profile_pic = null,
+    title = null,
+    profile_name = null,
+    view_count = null,
+    time_past = null
+  ) {
+    this.src_thumbnail = src_thumbnail;
+    this.src_profile_pic = src_profile_pic;
+    this.title = title;
+    this.profile_name = profile_name;
+    this.view_count = view_count;
+    this.time_past = time_past;
+  }
+}
+
+main();
+
+function main(){
+  // Start script
+
+  let vid = new VideoObj(); 
+  for (let item of videoList){
+    vid.src_thumbnail=item["src_thumbnail"];
+    vid.src_profile_pic=item["src_profile_pic"];
+    vid.title=item["title"];
+    vid.profile_name=item["profile_name"];
+    vid.view_count=item["view_count"];
+    vid.time_past=item["time_past"];
+    CreateVideo(vid);
+  }
+  for (let item of reelList){
+    CreateReel(item);
+  }
+  
+
+
+
+
+
+  // End script
+}
+function CreateReel(reelListData){
+
+  
+  // access the short_list
+  let div_short_list = document.querySelector("#short_list");
+
+  // create the card short wrapper with its attributes
+  let div_wrap_short = document.createElement("div");
+  div_wrap_short.setAttribute("class", "main_short_wrapper");
+
+  // append the short card into the list
+  div_short_list.appendChild(div_wrap_short);
+
+  // create the card short wrapper with its attributes
+  let div_wrap_short_thumbnail = document.createElement("div");
+  div_wrap_short_thumbnail.setAttribute("class", "main_short_thumbnail_wrapper");
+
+  // append the short card into the list
+  div_wrap_short.appendChild(div_wrap_short_thumbnail);
+
+  let img_short_thumbnail = document.createElement("img");
+  img_short_thumbnail.setAttribute("src", reelListData["thumbnail"]);
+  img_short_thumbnail.setAttribute("class", "main_short_thumbnail");
+
+  // append the video thumbnail into the card video
+  div_wrap_short_thumbnail.appendChild(img_short_thumbnail);
+  
+  let p_short_title = document.createElement("p");
+  p_short_title.innerHTML = reelListData["title"];
+  p_short_title.setAttribute("class", "main_short_title");
+  div_wrap_short.appendChild(p_short_title);
+  
+  /*
+      <div id="short_list">
+        <div class="main_short_wrapper">
+          <div class="main_short_thumbnail_wrapper">
+            <img src="imgs/reels/thumbnails/v1_steering_wheel_reel_thumbnail.jpg" class="main_short_thumbnail">
+          </div>
+          <p class="main_short_title">
+            Which one?🤔Logitech G920/G29/G25/GT/Pro, Thrustmaster T150, Fanatec GT3 #granturismo
+          </p>
+
+
+          <p class="main_short_profile_stats">
+            <span class="view_count_short_id"> 1.3M </span> views
+          </p>
+        </div>
+      </div>
+  */
+
+  ///// 
+  let p_profile_stats = document.createElement("p");
+  p_profile_stats.setAttribute("class", "main_short_profile_stats");
+  div_wrap_short.appendChild(p_profile_stats);
+
+  let span_view_count = document.createElement("span");
+  span_view_count.setAttribute("class", "view_count_short_id");
+  span_view_count.innerHTML = reelListData["view_count"] + " views ";
+  p_profile_stats.appendChild(span_view_count);
+}
+
+function CreateVideo(videoListData) {
+  // access the video_list
+  let div_video_list = document.querySelector("#video_list");
+
+  // create the card video wrapper with its attributes
+  let div_wrap = document.createElement("div");
+  div_wrap.setAttribute("class", "main_video_wrapper");
+
+  // append the video card into the list
+  div_video_list.appendChild(div_wrap);
+
+  // create the thumbnail with its attributes
+  let img_thumbnail = document.createElement("img");
+  img_thumbnail.setAttribute("src", videoListData["src_thumbnail"]);
+  img_thumbnail.setAttribute("class", "main_video_thumbnail");
+
+  // append the video thumbnail into the card video
+  div_wrap.appendChild(img_thumbnail);
+
+  let div_wrap_sec = document.createElement("div");
+  div_wrap_sec.setAttribute("class", "main_video_wrapper_second");
+  div_wrap.appendChild(div_wrap_sec);
+
+  let img_profile_pic = document.createElement("img");
+  img_profile_pic.setAttribute("src", videoListData["src_profile_pic"]);
+  img_profile_pic.setAttribute("class", "main_video_profile_pic");
+  div_wrap_sec.appendChild(img_profile_pic)
+
+  let div_wrap_third = document.createElement("div");
+  div_wrap_third.setAttribute("class", "main_video_wrapper_third");
+  div_wrap_sec.appendChild(div_wrap_third);
+
+  let p_title = document.createElement("p");
+  p_title.innerHTML = videoListData["title"];
+  p_title.setAttribute("class", "main_video_title");
+  div_wrap_third.appendChild(p_title);
+
+  let p_profile_name = document.createElement("p");
+  p_profile_name.setAttribute("class", "main_video_profile_name");
+  p_profile_name.innerHTML = videoListData["profile_name"];
+  div_wrap_third.appendChild(p_profile_name);
+
+
+  let p_profile_stats = document.createElement("p");
+  p_profile_stats.setAttribute("class", "main_video_profile_stats");
+  div_wrap_third.appendChild(p_profile_stats);
+
+  let span_view_count = document.createElement("span");
+  span_view_count.setAttribute("class", "view_count_id");
+  span_view_count.innerHTML = videoListData["view_count"] + " views &bull; ";
+  p_profile_stats.appendChild(span_view_count);
+
+  let span_time_past = document.createElement("span");
+  span_time_past.setAttribute("class", "time_past_id");
+  span_time_past.innerText = videoListData["time_past"] + " ago";
+  p_profile_stats.appendChild(span_time_past);
+
+}
---
+++ src/js/data.js
@@ -0,0 +1,106 @@
+let videoList = [{
+    "src_thumbnail": "imgs/thumbnails/v1_steering_wheel_thumbnail.jpg",
+    "src_profile_pic": "imgs/profile_pics/v1_steering_wheel_profile_pic.jpg",
+    "title": "V23 RACING WHEEL",
+    "profile_name": "MTM Industrial",
+    "view_count": "470",
+    "time_past": "2 days",
+  },
+  {
+    "src_thumbnail": "imgs/thumbnails/v2_hazel_scene_runtime_thumbnail.jpg",
+    "src_profile_pic": "imgs/profile_pics/v2_hazel_scene_runtime_profile_pic.jpg",
+    "title": "Playing and Stopping Scenes (and Resetting) // Game Engine series",
+    "profile_name": "The Cherno",
+    "view_count": "23K",
+    "time_past": "2 years",
+  },
+  {
+    "src_thumbnail": "imgs/thumbnails/v3_cheap_usb_handbrake_thumbnail.jpg",
+    "src_profile_pic": "imgs/profile_pics/v3_cheap_usb_handbrake_profile_pic.jpg",
+    "title": "I Bought The Cheapest USB Handbrake for my Logitech G29",
+    "profile_name": "Goosiest",
+    "view_count": "1.2M",
+    "time_past": "3 years",
+  },
+  {
+    "src_thumbnail": "imgs/thumbnails/v4_nfs_unbound_soundtrack_thumbnail.jpg",
+    "src_profile_pic": "imgs/profile_pics/v4_nfs_unbound_soundtrack_profile_pic.jpg",
+    "title": "Diplo - Worry No More (feat. Lil Yachty & Santigold) | Need for Speed Unbound SOUNDTRACK",
+    "profile_name": "LOUD",
+    "view_count": "130K",
+    "time_past": "1 year",
+  },
+  {
+    "src_thumbnail": "imgs/thumbnails/v5_writing-text-editor_thumbnail.jpg",
+    "src_profile_pic": "imgs/profile_pics/v5_writing-text-editor_profile_pic.jpg",
+    "title": "Writing Text Editor",
+    "profile_name": "Tsoding Daily",
+    "view_count": "50K",
+    "time_past": "2 years",
+  },
+  {
+    "src_thumbnail": "imgs/thumbnails/v6_race_music_thumbnail.jpg",
+    "src_profile_pic": "imgs/profile_pics/v6_race_music_profile_pic.jpg",
+    "title": "ＭＩＤＮＩＧＨＴ ＲＵＮ - Phonk Night Drive Music 2022 - BEST LXST CENTURY TYPE - CAR RACE MUSIC MIX 2022",
+    "profile_name": "Chill Beat",
+    "view_count": "1.5M",
+    "time_past": "1 year",
+  },
+  {
+    "src_thumbnail": "imgs/thumbnails/v7_backend_thumbnail.jpg",
+    "src_profile_pic": "imgs/profile_pics/v7_backend_profile_pic.jpg",
+    "title": "Backend web development - a complete overview",
+    "profile_name": "SuperSimpleDev",
+    "view_count": "1.5M",
+    "time_past": "2 years",
+  },
+  {
+    "src_thumbnail": "imgs/thumbnails/v8_java_opengl_thumbnail.jpg",
+    "src_profile_pic": "imgs/profile_pics/v8_java_opengl_profile_pic.jpg",
+    "title": "OpenGL 3D Game Tutorial 32: Font Rendering",
+    "profile_name": "ThinMatrix",
+    "view_count": "77K",
+    "time_past": "8 years",
+  },
+  {
+    "src_thumbnail": "imgs/thumbnails/v9_blender_scene_thumbnail.jpg",
+    "src_profile_pic": "imgs/profile_pics/v9_blender_scene_profile_pic.jpg",
+    "title": "Made with Blender - Town (Stylize) - Timelapse",
+    "profile_name": "Attar J",
+    "view_count": "643K",
+    "time_past": "7 months",
+  },
+  {
+    "src_thumbnail": "imgs/thumbnails/v10_drift_japan_thumbnail.jpg",
+    "src_profile_pic": "imgs/profile_pics/v10_drift_japan_profile_pic.jpg",
+    "title": "Drifting in Japan on the NEW Fukuoka Map in Assetto Corsa | Fanatec CSL DD",
+    "profile_name": "Project Sim Racing",
+    "view_count": "1.7M",
+    "time_past": "4 months",
+  }
+];
+
+
+let reelList = [
+  {
+    "thumbnail": "imgs/reels/thumbnails/v1_steering_wheel_reel_thumbnail.jpg",
+    "profile_pic": "imgs/reels/profile_pics/v1_steering_wheel_reel_profile_pic.jpg",
+    "username": "@nixon_game",
+    "title":"Which one?🤔Logitech G920/G29/G25/GT/Pro, Thrustmaster T150, Fanatec GT3 #granturismo",
+    "view_count": "1.3M"
+  }, 
+  {
+    "thumbnail": "imgs/reels/thumbnails/v1_steering_wheel_reel_thumbnail.jpg",
+    "profile_pic": "imgs/reels/profile_pics/v1_steering_wheel_reel_profile_pic.jpg",
+    "username": "@nixon_game",
+    "title":"Which one?🤔Logitech G920/G29/G25/GT/Pro, Thrustmaster T150, Fanatec GT3 #granturismo",
+    "view_count": "1.3M"
+  },
+  {
+    "thumbnail": "imgs/reels/thumbnails/v1_steering_wheel_reel_thumbnail.jpg",
+    "profile_pic": "imgs/reels/profile_pics/v1_steering_wheel_reel_profile_pic.jpg",
+    "username": "@nixon_game",
+    "title":"Which one?🤔Logitech G920/G29/G25/GT/Pro, Thrustmaster T150, Fanatec GT3 #granturismo",
+    "view_count": "1.3M"
+  },
+];
